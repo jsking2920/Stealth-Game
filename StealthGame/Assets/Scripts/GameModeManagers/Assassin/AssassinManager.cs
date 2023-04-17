5ddb5c90d9086efcf284d87c1b6b85daf9691024
ama28 <amm28y@gmail.com>
2023-04-17 11:24:30 -0400

    updated hitmen victim mark
    still needs work tho it kinda sucks

--- StealthGame/Assets/Scripts/GameModeManagers/Assassin/AssassinManager.cs
+++ StealthGame/Assets/Scripts/GameModeManagers/Assassin/AssassinManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityMovementAI;
+using Vector3 = System.Numerics.Vector3;
 
 public class AssassinManager : TimedGameMode
 {
