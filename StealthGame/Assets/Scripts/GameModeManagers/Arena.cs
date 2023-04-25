2f0addcc1a11b7d2f076124e1a535e11b338a2f1
Scott King <jsking@andrew.cmu.edu>
2023-04-24 20:35:25 -0400

    planet spinning and ui scaling

--- StealthGame/Assets/Scripts/GameModeManagers/Arena.cs
+++ StealthGame/Assets/Scripts/GameModeManagers/Arena.cs
@@ -1,8 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Arena : MonoBehaviour
 {
     public UnityMovementAI.MovementAIRigidbody[] obstacles;
+
+    public float rotationSpeed;
+    public Transform[] rotatingObjects;
+
+    public void Update()
+    {
+        foreach (Transform t in rotatingObjects)
+        {
+            t.Rotate(new Vector3(0.0f, 0.0f, 1.0f * rotationSpeed));
+        }
+    }
 }
